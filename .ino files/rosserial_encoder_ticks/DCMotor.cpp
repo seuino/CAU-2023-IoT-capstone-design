fbfb72e89d7c55ce5783e2d91db25b4f23df5cd0
seuino <parksb1109@gmail.com>
2023-08-01 16:18:51 +0900

    Added encoder_ticks.ino(including header file)

---
+++ .ino files/rosserial_encoder_ticks/DCMotor.cpp
@@ -0,0 +1,38 @@
+#include "DCMotor.h"
+// .cpp
+//////////////////////////////////////////////////
+#define GEAR_RATIO 986.61
+#define CPR 12
+DCMotor::DCMotor(uint8_t motorA_pin, uint8_t motorB_pin, uint8_t enable_pin) {
+
+  _motorA_pin = motorA_pin;
+  _motorB_pin = motorB_pin;
+  _enable_pin = enable_pin;
+
+}
+void DCMotor::control(int key, uint8_t pwm) {
+
+  degree = float(encoder_ticks) * 360/GEAR_RATIO/CPR;
+
+  analogWrite(_enable_pin, pwm);
+  switch(key) {
+    case 1:
+      digitalWrite(_motorA_pin,HIGH);
+      digitalWrite(_motorB_pin,LOW);
+      break;
+    case 2:
+      digitalWrite(_motorA_pin,LOW);
+      digitalWrite(_motorB_pin,HIGH);
+      break;
+    case 3:
+      digitalWrite(_motorA_pin,HIGH);
+      digitalWrite(_motorB_pin,HIGH);
+      break;
+  }
+
+  // For monitoring in arduino
+  // Serial.println(degree);
+
+  // For monitoring in python
+
+}
